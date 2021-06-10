be491d95b578d92e8dc0b7a69f47a4f0a0133833
Trevor <trevoranthonylane@gmail.com>
2021-06-09 22:12:03 -0700

    add text

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -433,9 +433,10 @@ const Hero = () => {
                 <span className="block text-red-600 xl:inline"> Legal Nurse Consultants</span>
               </h1>
               <p className="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+                A coordinated team of legal nurse consultants working as your medical subject matter experts. 
+                
               </p>
-              <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
+              {/* <div className="mt-10 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a href="/" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                     Get started
@@ -446,7 +447,7 @@ const Hero = () => {
                     Live demo
                   </a>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
 
