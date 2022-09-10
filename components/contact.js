51f2028cf9429f0ce12f5a61e1e868b6dc66f58d
Trevor <trevoranthonylane@gmail.com>
2022-09-10 16:00:31 -0600

    updates

--- components/contact.js
+++ components/contact.js
@@ -7,8 +7,8 @@ export default function Component(props){
         <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
           <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
             <div className="max-w-lg mx-auto">
-              <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
-                Get in touch
+              <h2 className="text-2xl font-extrabold tracking-tight text-red-600 sm:text-3xl">
+                Call Us Today
               </h2>
               <p className="mt-3 text-lg leading-6 text-gray-500">
               </p>
@@ -24,10 +24,10 @@ export default function Component(props){
                   <dt className="sr-only">Phone number</dt>
                   <dd className="flex">
                     {/* Heroicon name: phone */}
-                    <svg className="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <svg className="flex-shrink-0 h-6 w-6 text-red-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
-                    <span className="ml-3">
+                    <span className="ml-3 text-red-500 font-bold">
                       +1 (559) 816-3948
                     </span>
                   </dd>
